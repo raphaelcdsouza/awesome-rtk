3ebd7693f73f4dc88d172b79b52b478d14e21b46
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2022-12-03 23:02:17 -0800

    Exporting errors and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './Infra/Facades';
+export * from './Infra/Interfaces/DB';
 export * from './Infra/Gateways';
+export * from './Infra/Interfaces/Gateways';
+export * from './Errors';
 export * from './Utils';
